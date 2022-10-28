e034f018af3377812909b7219e4089428bf3806d
Gerv-G <25771885+Gerv-G@users.noreply.github.com>
2022-10-28 14:08:11 +0800

    Add dependencies for Spring Cloud + AWS Lambda

--- src/main/java/io/gervg/learnawslambda/LearnAwsLambdaApplication.java
+++ src/main/java/io/gervg/learnawslambda/LearnAwsLambdaApplication.java
@@ -2,12 +2,29 @@ package io.gervg.learnawslambda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import java.util.function.Function;
+
+/**
+ * Taken from the example in spring-cloud-function
+ * see https://github.com/spring-cloud/spring-cloud-function/tree/main/spring-cloud-function-samples/function-sample-aws
+ */
 @SpringBootApplication
 public class LearnAwsLambdaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearnAwsLambdaApplication.class, args);
+	public static void main(String[] args) {}
+
+	@Bean
+	public Function<String, String> upperCase() {
+		return value -> {
+			if (value.equals("exception")) {
+				throw new RuntimeException("Intentional exception");
+			}
+			else {
+				return value.toUpperCase();
+			}
+		};
 	}
 
 }
