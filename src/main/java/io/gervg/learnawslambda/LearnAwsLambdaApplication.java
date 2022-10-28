cb04a5d1b5a9b9f54d0d3869f954131349d28022
Gerv-G <25771885+Gerv-G@users.noreply.github.com>
2022-10-28 11:50:17 +0800

    Initial commit - base Spring app

---
+++ src/main/java/io/gervg/learnawslambda/LearnAwsLambdaApplication.java
@@ -0,0 +1,13 @@
+package io.gervg.learnawslambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAwsLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAwsLambdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/gervg/learnawslambda/LearnAwsLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gervg.learnawslambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnAwsLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
